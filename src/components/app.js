d82de9d47842deca03cfe75420e7b937c30944a0
ken.chen <ken.chen@solentive.com>
2018-02-09 17:13:35 +1100

    add app

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import PropTypes from 'prop-types';
+import Slide from './slide';
 
  
-class App extends Component {
+class App extends React.Component {  
   render() {
+    const {carousels} = this.props; 
+    debugger;   
     return (
       <div className="App">
-        test 12       
+        {/* <ul>
+          { carousels.map((carousel => {            
+            <Slide />;
+          }))}                
+        </ul> */}
+        {
+          carousels.map((carouse => {
+            //<Slide />;
+            <div>test</div>;
+          }))          
+        }        
       </div>
     );
   }
 }
 
-function mapStateToProps(state, ownProps){
-  debugger;
+function mapStateToProps(state, ownProps){  
   return {
     carousels: state.carousels
   };
+}
+
+App.propTypes = {
+  carousels: PropTypes.array.isRequired
 }
 
 export default connect(mapStateToProps)(App);
---
+++ src/components/slide.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Slide extends Component {  
+  
+  render() {
+    debugger;
+    return (
+      <div>        
+        test
+      </div>
+    );
+  }
+}
+
+export default Slide;
